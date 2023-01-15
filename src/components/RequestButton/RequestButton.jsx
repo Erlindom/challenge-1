11046eef473856cb28070187f0c4bf45da2141cc
Erlin Dominguez <erlind299@gmail.com>
2023-01-15 17:32:42 -0500

    Create component request button

---
+++ src/components/RequestButton/RequestButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./RequestButton.css";
+
+function RequestButton() {
+  return (
+    <>
+      <a className="request-btn" href="/#">
+        Request invite
+      </a>
+    </>
+  );
+}
+
+export default RequestButton;
